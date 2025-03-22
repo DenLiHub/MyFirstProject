bcb8f26dccd2a83111367ef1ec2b947ba336462a
Denis Likhachev <denliemail@yandex.ru>
2025-03-22 19:09:22 +0300

    Проверка коммита 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Проверка коммита"); //
-        // new line
+        // Коммит с файлом
     }
 }
